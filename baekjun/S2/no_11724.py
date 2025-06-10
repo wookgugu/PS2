de664c3a890a1dd2961dc6d2ac5a5b1eb327757f
wookgugu <wldud990915@naver.com>
2025-06-10 13:07:07 +0000

    DFS/BFS 수행횟수 = 연결요소의 개수

--- baekjun/S2/no_11724.py
+++ baekjun/S2/no_11724.py
@@ -4,6 +4,13 @@
 # M개의 줄에 간선의 양 끝점 u와 v가 주어진다. (1 ≤ u, v ≤ N, u ≠ v) 같은 간선은 한 번만 주어진다.
 # 첫째 줄에 연결 요소의 개수를 출력한다.
 
+# dfs 또는 bfs를 수행하는 횟수 : 연결 요소의 개수
+
+
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 graph = [[]*(N+1)for _ in range(N+1)]
 for i in range(M) :
@@ -14,4 +21,42 @@ for i in range(M) :
 for i in graph:
     i.sort()
 
-# dfs 또는 bfs를 수행하는 횟수 : 연결 요소의 개수
+
+# 1 DFS 이용
+# dfs 함수
+def dfs(graph, v, visited) :
+    visited[v] = True
+    for i in graph[v] :
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+count = 0 # 연결 노드의 수
+visited = [False] * (N+1)
+for i in range(1,N+1) :
+    if not visited[i]:
+        dfs(graph, i, visited)
+        count += 1
+print(count)
+
+
+# 2. BFS 이용
+# bfs 함수
+from collections import deque
+def bfs(graph, start, visited) :
+    queue = deque([start])
+    visited(start) = True
+
+    while queue:
+        v = queue.popleft()
+        for i in graph[v] :
+            if not visited[v] :
+                queue.append(i)
+                visited[i] = True
+
+count = 0 # 연결 노드의 수
+visited = [False] * (N+1)
+for i in range(1,N+1) :
+    if not visited[i]:
+        bfs(graph, i, visited)
+        count += 1
+print(count)
