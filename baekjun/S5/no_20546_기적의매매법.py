965fdfb278e8f0384215ad7e8cab00b32f4ee7b3
wookgugu <wldud990915@naver.com>
2025-06-13 01:40:08 +0000

    answer

--- baekjun/S5/no_20546_기적의매매법.py
+++ baekjun/S5/no_20546_기적의매매법.py
@@ -1,2 +1,44 @@
-def jun(money, jusic) :
-    
+money = int(input())
+juga = list(map(int, input().split()))
+
+def jun(money, juga) :
+    jusic = 0
+    for j in juga :
+        if money == 0 :
+    #        print(jusic)
+            continue
+        jusic += (money // j)
+        money = (money % j)
+    #    print(jusic)
+    return money + juga[-1]*jusic
+
+def seong(money, juga) :
+    jusic = 0 
+    j_1 = juga[0]
+    plus, minus = 0, 0
+    for j in juga[1:] :
+        if j_1 < j : # 상승
+            plus += 1  # 상승 스택
+            minus = 0  # 하락 초기화
+        elif j_1 > j :  # 하락
+            plus = 0   # 상승 초기화
+            minus += 1 # 하락 스택
+        j_1 = j
+        if plus == 3 : # 상승 3스택
+            money += j * jusic
+            jusic = 0
+            plus = 0
+        if minus == 3 : # 하락 3스택
+            jusic += (money // j)
+            money = (money % j)
+            minus = 0
+    return money + juga[-1]*jusic
+
+j_money = jun(money, juga)
+s_money = seong(money, juga)
+if j_money > s_money :
+    print('BNP')
+elif j_money < s_money :
+    print('TIMING')
+else :
+    print('SAMESAME')
