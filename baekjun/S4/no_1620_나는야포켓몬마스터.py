4119168ee971fc9e0803385b4ae2ad3fe89b17dc
wookgugu <wldud990915@naver.com>
2025-06-16 13:46:40 +0000

    dic 두개 or dic+list

--- baekjun/S4/no_1620_나는야포켓몬마스터.py
+++ baekjun/S4/no_1620_나는야포켓몬마스터.py
@@ -15,3 +15,23 @@ for i in range(m) :
 
 for a in ans :
     print(a)
+# 시간 5304ms는 너무 긴거 아닌가...?
+
+n, m = map(int, input().split())
+n_dic = {}
+n_list = [0]
+ans = []
+for i in range(1,n+1) :
+    name = input()
+    n_list.append(name)
+    n_dic[name] = i
+
+for i in range(m) :
+    q = input().rstrip()
+    if q.isdigit() :
+        ans.append(n_list[int(q)])
+    elif q.isalpha() :
+        ans.append(n_dic[q])
+
+for a in ans :
+    print(a)
