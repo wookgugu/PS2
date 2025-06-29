883584eadfac3b94d35bbf35d8973f19e3f6ffdd
wookgugu <wldud990915@naver.com>
2025-06-29 09:07:10 +0000

    엥모르겠다

---
+++ baekjun/S4/no_1755_숫자놀이.py
@@ -0,0 +1,9 @@
+num = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five',
+       6:'six', 7:'seven', 8:'eight', 9:'nine', 0:'zero'}
+
+M, N = map(int, input().split())
+
+
+
+# for i in range(M,N+1) :
+#     for j in i.split() :
